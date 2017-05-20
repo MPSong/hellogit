74faec7be7eed6f20ce41954a4609489370159a0
hyominK <hyomin911@naver.com>
2017-05-21 03:03:14 +0900

    Add files via upload

---
+++ Client/mainwindow.cpp
@@ -0,0 +1,26 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "dialog.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_9_clicked()
+{
+     Dialog dia;
+     dia.exec();
+}
+
+void MainWindow::on_pushButton_8_clicked()
+{
+
+}
