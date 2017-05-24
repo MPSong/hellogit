18324dbba2ecf19de36230596272d1081a9ef30e
hyominK <hyomin911@naver.com>
2017-05-25 02:50:37 +0900

    Add files via upload

--- Client/mainwindow.h
+++ Client/mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtSql>
+#include <QDebug>
+#include <QFileInfo>
 
 namespace Ui {
 class MainWindow;
@@ -16,12 +19,27 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_pushButton_8_clicked();
+    void on_pushButton_signin_clicked();
 
-    void on_pushButton_9_clicked();
+    void on_pushButton_makeroom_clicked();
+
+    void on_pushButton_signup_clicked();
+
+    void on_pushButton_cancel_clicked();
+
+    void on_pushButton_logout_clicked();
+
+    void on_pushButton_logout_2_clicked();
+    
+    void on_pushButton_confirm_clicked();
+
+    void on_pushButton_check_clicked();
+
+    void on_pushButton_check_2_clicked();
 
 private:
     Ui::MainWindow *ui;
+    QSqlDatabase mydb;
 };
 
 #endif // MAINWINDOW_H
